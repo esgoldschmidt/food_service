6d71efde297765be17f8d5bbb2e2671db73e4f63
Chukwudi Nweze <45886893+chukwudinweze@users.noreply.github.com>
2023-10-30 21:49:44 +0100

    modified todo-item

--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -25,10 +25,10 @@ const TodoItem = ({ title, id, handleEdit }: TodoItemProps) => {
         <button onClick={editTask} className="px-1">
           <Pencil className="text-slate-500" />
         </button>
-        <button className="px-1" type="submit">
+        <button onClick={completeTodo} className="px-1" type="submit">
           <CheckCircle className="text-green-300" />
         </button>
-        <button className="px-1" type="submit">
+        <button onClick={deleteTask} className="px-1" type="submit">
           <Trash2 className="text-red-600" />
         </button>
       </div>
